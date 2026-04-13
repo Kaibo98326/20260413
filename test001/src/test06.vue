ff7e4cb0fe3f95a6a2468571a5ce0b24e2eb940b
Kaibo <kaibo98326@gmail.com>
2026-04-13 15:21:39 +0800

    shift + ctrl 送出表單

--- test001/src/test06.vue
+++ test001/src/test06.vue
@@ -1,3 +1,37 @@
-<script setup></script>
-<template></template>
-<style scoped></style>
+<script setup>
+    const handleSubmit = (e) => {
+        // e.preventDefault();
+        console.log("wwww");
+        
+    }
+</script>
+<template>
+    <form @submit.prevent>
+        <div class="row">
+            <label for="userId">ID : </label>
+            <input type="text" id="userId" name="userId" placeholder="輸入使用者帳號" @keydown.shift.enter.prevent="handleSubmit">
+        </div>
+        <div class="row">
+            <label for="userPWD">Password : </label>
+            <input type="password" id="userPWD" name="userPWD" placeholder="輸入使用者密碼" @keydown.shift.enter.prevent="handleSubmit">
+        </div>
+        <div class="ctrl">
+            <button @click="handleSubmit">送出</button>
+        </div>
+    </form>
+</template>
+<style scoped>
+.row{
+     display: flex;
+
+     input{
+        flex: 1 ;
+     }
+}
+.ctrl{
+    display: flex;
+    justify-content: flex-end;
+    
+}
+
+</style>
